3f592daffb6f4ba5e71020c2a53cb81c6ba19964
Kaushal B <36352020+bkaushal07@users.noreply.github.com>
2018-11-21 23:03:35 +0530

    Indented well

--- lab8a.php
+++ lab8a.php
@@ -1,42 +1,44 @@
 <!DOCTYPE html>
 <html>
-<head>
-<title>Calculator</title>
-</head>
-<body>
-<div class="container" style="margin-top: 50px">
-<?php
-// If the submit button has been pressed
-if(isset($_POST['submit']))
-{
-// Check number values
-if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
-{
-// Calculate total
-if($_POST['operation'] == 'plus')
-{
-$total = $_POST['number1'] + $_POST['number2'];
-}
-if($_POST['operation'] == 'minus')
-{
-$total = $_POST['number1'] - $_POST['number2'];
-}
-if($_POST['operation'] == 'times')
-{
-$total = $_POST['number1'] * $_POST['number2'];
-}
-if($_POST['operation'] == 'divided by')
-{
-$total = $_POST['number1'] / $_POST['number2'];
-}
-// Print total to the browser
-echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
-} else {
-// Print error message to the browser
-echo 'Numeric values are required';
-}
-}
-?>
+    <head>
+        <title>Calculator</title>
+    </head>
+    <body>
+    <div class="container" style="margin-top: 50px">
+    <?php
+    // If the submit button has been pressed
+        if(isset($_POST['submit']))
+        {
+        // Check number values
+            if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
+            {
+            // Calculate total
+            if($_POST['operation'] == 'plus')
+            {
+                $total = $_POST['number1'] + $_POST['number2'];
+            }
+            if($_POST['operation'] == 'minus')
+            {
+                $total = $_POST['number1'] - $_POST['number2'];
+            }
+            if($_POST['operation'] == 'times')
+            {
+                $total = $_POST['number1'] * $_POST['number2'];
+            }
+            if($_POST['operation'] == 'divided by')
+            {
+                $total = $_POST['number1'] / $_POST['number2'];
+            }
+            // Print total to the browser
+            echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
+            } 
+            else 
+            {
+            // Print error message to the browser
+            echo 'Numeric values are required';
+            }
+        }
+    ?>
 <!-- Calculator form -->
 <form method="post" action="lab8a.php">
 <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
--- lab8b.php
+++ lab8b.php
@@ -21,19 +21,21 @@ $rowCount= count($matrix1); //Provides the rowcount of matrix
 $colCount = count($matrix1[0]); //Provides the column count of matrix
 echo "The input matrix A is:\n";
 for($r = 0; $r < $rowCount; $r++)
-{ for($c=0; $c < $colCount; $c++)
 {
-echo $matrix1[$r][$c]." \t";
-}
-echo "\n";
+    for($c=0; $c < $colCount; $c++)
+    {
+        echo $matrix1[$r][$c]." \t";
+    }
+    echo "\n";
 }
 echo "The input matrix B is:\n";
 for($r = 0; $r < $rowCount; $r++)
-{ for($c=0; $c < $colCount; $c++)
 {
-echo $matrix2[$r][$c]." \t";
-}
-echo "\n";
+    for($c=0; $c < $colCount; $c++)
+    {
+        echo $matrix2[$r][$c]." \t";
+    }
+    echo "\n";
 }
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 //The transpose of the matrix
@@ -41,11 +43,12 @@ echo "\nThe output Transpose of matrix is:\n";
 
 
 for($r = 0; $r < $colCount; $r++)
-{ for($c=0; $c < $rowCount; $c++)
 {
-echo $matrix1[$c][$r]." \t";
-}
-echo "\n";
+    for($c=0; $c < $rowCount; $c++)
+    {
+        echo $matrix1[$c][$r]." \t";
+    }
+    echo "\n";
 }
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 $rowCount= count($matrix1); //Provides the rowcount of matrix
@@ -55,39 +58,41 @@ $colCount2 = count($matrix2[0]);
 //The sum of the matrix
 echo "\nThe sum of matrix is:\n";
 for($r = 0; $r < $rowCount; $r++)
-{ for($c=0; $c < $colCount; $c++)
 {
-$val= $matrix1[$r][$c] + $matrix2[$r][$c];
-echo $val."\t";
-}
-echo "\n";
+    for($c=0; $c < $colCount; $c++)
+    {
+        $val= $matrix1[$r][$c] + $matrix2[$r][$c];
+        echo $val."\t";
+    }
+    echo "\n";
 }
 //$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 /*$rowCount= count($matrix1); //Provides the rowcount of matrix
 $colCount = count($matrix1[0]); //Provides the column count of matrix
 $rowCount2 = count($matrix2);
 $colCount2 = count($matrix2[0]);*/
-//The Multiplication of the matrix
+
+
 echo "\nThe Multiplication of matrix is:\n";
 //A*B C*D
 //B is not equal to C
 if($colCount == $rowCount2)
 {
-for($r = 0; $r < $rowCount; $r++)
-{ for($c=0; $c < $colCount; $c++)
-{
-$val=0;
-for($k=0;$k<$colCount;$k++)
-{
-$val= $val+$matrix1[$r][$k] * $matrix2[$k][$c];
-}
-echo $val."\t";
-}
-echo "\n";
-}
+    for($r = 0; $r < $rowCount; $r++)
+    {
+        for($c=0; $c < $colCount; $c++)
+        {
+            $val=0;
+            for($k=0;$k<$colCount;$k++)
+            {
+                $val= $val+$matrix1[$r][$k] * $matrix2[$k][$c];
+            }
+            echo $val."\t";
+        }
+    echo "\n";
+    }
 }
 else
-
 {
 echo "The matrix multiplication is not possible.";
 }
